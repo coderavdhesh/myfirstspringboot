872788f22b67187c5e3c1a78718051419784f656
avdhesh_gupta <avdheshgupta1607@gmail.com>
2022-12-01 18:53:54 +0530

    final commit

---
+++ src/main/java/com/avdhesh/practice/myfirstspringboot/MyfirstspringbootApplication.java
@@ -0,0 +1,16 @@
+package com.avdhesh.practice.myfirstspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class MyfirstspringbootApplication {
+
+		public static void main(String[] args) {
+		SpringApplication.run(MyfirstspringbootApplication.class, args);
+
+		System.out.println("Avdhesh : My project now started.");
+	}
+
+}
---
+++ src/test/java/com/avdhesh/practice/myfirstspringboot/MyfirstspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avdhesh.practice.myfirstspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfirstspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
